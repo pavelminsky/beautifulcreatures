9e3dd100b02dcea1f2d0b35daf0b3be5729a4b20
Pavel Minsky <pavelminsky@gmail.com>
2013-02-06 15:31:41 +0400

    index_done

---
+++ header_include.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head><title>Прекрасные создания — в кино с 14 февраля</title>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="Прекрасные создания. Официальный сайт фильма. В кино с 14 февраля.">
+<meta name="keywords" content="Прекрасные создания, Beautiful Creatures, Джереми Айронс, Эмма Томпсон, Эмми Россум, Томас Манн, Виола Дэвис, Ками Гарсия, Маргарет Штоль, драма, мистика, книга, Beautiful Darkness, Beautiful Chaos, Beautiful Redemption">
+
+<meta property="og:title" content="Прекрасные создания - официальный сайт фильма">
+<meta property="og:description" content="Прекрасные создания – мистическая драма, основанная на одноименном книжном бестселлере Ками Гарсиа и Маргарет Штоль">
+<meta property="og:type" content="movie">
+<meta property="og:url" content="http://www.beautifulcreaturesmovie.ru">
+<meta property="og:image" content="http://www.beautifulcreaturesmovie.ru/images/share.jpg">
+<meta property="og:site_name" content="Прекрасные создания - официальный сайт фильма">
+
+<meta name="viewport" content="width=990">
+<meta name="format-detection" content="telephone=no">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-status-bar-style" content="black">
+<meta name="apple-mobile-web-app-title" content="Прекрасно">
+<link rel="apple-touch-icon" href="apple-touch-icon.png">
+<link rel="apple-touch-icon-precomposed" href="apple-touch-icon.png">
+<link rel="shortcut icon" href="http://www.beautifulcreaturesmovie.ru/favicon.ico" type="image/x-icon">
+<link rel="icon" href="http://www.beautifulcreaturesmovie.ru/favicon.ico" type="image/x-icon">
+
+<meta itemprop="name" content="Прекрасные создания - официальный сайт фильма">
+<meta itemprop="description" content="Прекрасные создания – мистическая драма, основанная на одноименном книжном бестселлере Ками Гарсиа и Маргарет Штоль">
+<meta itemprop="image" content="http://www.beautifulcreaturesmovie.ru/images/share.jpg">
+
+<link rel="stylesheet" href="css/main.css">
+<link rel="stylesheet" type="text/css" href="css/fancybox.css?v=2.1.1" media="screen">
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<script type="text/javascript" src="js/init.js"></script>
+<script>
+jQuery(function($) {
+$('.dim').fadeOut(500);
+});
+</script>
+
+<link href="css/video-js.css" rel="stylesheet">
+<script type="text/javascript" src="js/video.js"></script>
+<style type="text/css">
+.vjs-default-skin .vjs-play-progress {background: #005f99;}
+.vjs-default-skin .vjs-load-progress {background: #204d62;}
+.vjs-default-skin .vjs-seek-handle {display: none;}
+.vjs-default-skin .vjs-current-time-display, .vjs-default-skin .vjs-remaining-time-display {font: normal 10px Arial; color: #647378; text-shadow: none;}
+</style>
+
+<script type="text/javascript">
+$(function(){
+  var viewPortWidth = $(window).width();
+  if (viewPortWidth > 1900) {$('body').addClass('extraWide')}
+  else if (viewPortWidth > 1400) {$('body').addClass('wide')}
+  else if (viewPortWidth > 1000) {$('body').addClass('standard')}
+  else if (viewPortWidth > 700) {$('body').addClass('narrow')}
+  else {$('body').addClass('extraNarrow')}
+});
+</script>
+
+</head><body>
---
+++ index.php
@@ -0,0 +1,103 @@
+<?php include ("header_include.php"); ?>
+
+<noscript><style>.dim {display: none;} .mainSlogan {display: none;} .newsList .newsbox {display: block;}</style><div style="position: absolute; width: 500px; height: 120px; z-index: 9999; top: -5px; left: 50%; margin-left: -250px;"><img src="images/noscript.png" alt="В вашем браузере отключен JavaScript"></div></noscript>
+<div class="dim">&nbsp;</div>
+
+
+
+<div class="mainWrap">
+	<img class="mainSlogan noSelect" src="images/mainSlogan.png">
+	<img class="mainLogo noSelect" src="images/mainLogo.png">
+	<img class="mainCast noSelect" src="images/mainCast.png">
+	<img class="mainBG noSelect" src="images/mainBG.jpg">
+	<div class="mainOverlay noSelect">&nbsp;</div>
+	<div class="indexMenu mainMenu">
+		<div class="menuFadeOut_l noSelect">&nbsp;</div>
+		<div class="menuButtons"><a href="javascript:void(0)" class="index active">Главная</a><a href="javascript:void(0)" class="news disabled fadeout">Новости</a><a href="javascript:void(0)" class="books disabled fadeout">Книги</a><a href="javascript:void(0)" class="about disabled fadeout">Фильм</a><a href="javascript:void(0)" class="team disabled fadeout">Команда</a><a href="javascript:void(0)" class="video disabled fadeout">Видео</a><a href="javascript:void(0)" class="gallery disabled fadeout">Кадры</a></div>
+		<div class="menuFadeOut_r noSelect">&nbsp;</div>
+	</div>
+</div>
+
+
+
+<div class="mainRoyal clearfix">
+	<div class="mainLocket noSelect">&nbsp;</div>
+	<div class="royalFadeOut_l noSelect">&nbsp;</div>
+
+	<div class="mainContent">
+		<div class="mainContent_lc">
+			<img class="date noSelect" src="images/mainDate.png"><br>
+			<img class="age noSelect" src="images/age.png">
+		</div>
+		<div class="mainContent_rc newsList">
+			<ul>
+			<?php include ("news_include.php"); ?>
+			</ul>
+			<!--p style="padding-top: 20px; font-size: 120%;"><a href="news.html">Все новости</a> &rarr;</p-->
+		</div>
+	</div>
+
+	<div class="royalFadeOut_r noSelect">&nbsp;</div>
+</div>
+
+
+
+<div class="copy">&copy; 2013 &laquo;<a href="http://www.westvideo.ru">Вест Видео</a>&raquo;. Создание сайта: <a href="http://www.kg-design.ru/">КГ-Дизайн</a></div>
+
+
+
+<div id="counters">
+	<div class="counters_list">
+	<p>А это наши прекрасные счетчики:</p>
+	<p>
+	<?php //include ("counters_include.php"); ?>	
+	</p>
+	</div>
+</div>
+
+
+
+
+
+
+<!-- Must be under news! -->
+<script type="text/javascript">
+jQuery(".newshd").click( function(){
+    var newsbox = jQuery(this).next();
+    if( newsbox.is(":visible") ){
+        newsbox.slideUp();   
+    }else{
+        newsbox.slideDown();   
+    }
+});
+</script>
+
+
+<!-- Fancybox -->
+<script type="text/javascript" src="js/jquery.fancybox.js?v=2.1.1"></script>
+<script type="text/javascript" src="js/jquery.fancybox-thumbs.js?v=1.0.7"></script>
+<script type="text/javascript" src="js/jquery.fancybox-media.js?v=1.0.4"></script>
+<script type="text/javascript">
+$(document).ready(function() {
+$('.gallery_news').fancybox({
+//prevEffect : 'none',
+//nextEffect : 'none',
+//closeBtn  : false,
+//arrows    : false,
+nextClick : false,
+helpers : {thumbs : {width  : 50, height : 50}}
+});
+$('.videos_news').fancybox({
+//prevEffect : 'none',
+//nextEffect : 'none',
+//closeBtn  : false,
+arrows    : false,
+nextClick : false,
+helpers : {thumbs : {width  : 90, height : 50}}
+});
+});
+</script>
+
+
+
+</body></html>
